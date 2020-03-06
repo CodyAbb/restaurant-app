47e375ae72fd2f48ff4705956469ada2fe80becd
Cody Abbott <cody.abbott16@gmail.com>
2020-03-06 13:30:29 +0000

    initial setup for both front and back

---
+++ back_end/src/main/java/com/codeclan/example/RestaurantBooker/RestaurantBookerApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.RestaurantBooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantBookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantBookerApplication.class, args);
+	}
+
+}
