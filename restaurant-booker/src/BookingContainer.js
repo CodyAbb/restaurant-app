17d91cdf889d04fbb35b5d48f118b586170770d1
Serenity911 <silvia.simonassi01@gmail.com>
2020-03-06 15:44:06 +0000

    added a simple form to add a booking

---
+++ restaurant-booker/src/BookingContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FormBox from './Form/FormBox.js'
+
+function BookingContainer() {
+    return (
+        <>
+        <p>Hello I am the booking container</p>
+        <FormBox />
+        </>
+    )
+  }
+  
+  export default BookingContainer;
+  
+
