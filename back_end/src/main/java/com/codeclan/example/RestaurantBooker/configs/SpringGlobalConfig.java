8992b57cc392002e4062fb2ca28f5aca1f1e4b20
Cody Abbott <cody.abbott16@gmail.com>
2020-03-06 16:29:26 +0000

    added cors override

---
+++ back_end/src/main/java/com/codeclan/example/RestaurantBooker/configs/SpringGlobalConfig.java
@@ -0,0 +1,34 @@
+package com.codeclan.example.RestaurantBooker.configs;
+
+import com.codeclan.example.RestaurantBooker.Models.Booking;
+import com.codeclan.example.RestaurantBooker.Models.Customer;
+import com.codeclan.example.RestaurantBooker.Models.Desk;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class SpringGlobalConfig implements RepositoryRestConfigurer, WebMvcConfigurer {
+    private static final String CORS_BASE_PATTERN = "/**";
+    private static final String ALLOWED_ORIGINS = "*";
+    private static final String ALLOWED_HEADERS = "*";
+    private static final String ALLOWED_METHODS = "*";
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Customer.class, Desk.class, Booking.class);
+        config.getCorsRegistry()
+                .addMapping(CORS_BASE_PATTERN)
+                .allowedOrigins(ALLOWED_ORIGINS)
+                .allowedHeaders(ALLOWED_HEADERS)
+                .allowedMethods(ALLOWED_METHODS);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping(CORS_BASE_PATTERN)
+                .allowedOrigins(ALLOWED_ORIGINS)
+                .allowedHeaders(ALLOWED_HEADERS)
+                .allowedMethods(ALLOWED_METHODS);
+    }
+}
