18ee0d83bb7093a44926f42284131d014f403204
Serenity911 <silvia.simonassi01@gmail.com>
2020-03-08 12:43:11 +0000

    created structure for table graph

--- back_end/src/test/java/com/codeclan/example/RestaurantBooker/RestaurantBookerApplicationTests.java
+++ back_end/src/test/java/com/codeclan/example/RestaurantBooker/RestaurantBookerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.codeclan.example.RestaurantBooker;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RestaurantBookerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.codeclan.example.RestaurantBooker;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class RestaurantBookerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
