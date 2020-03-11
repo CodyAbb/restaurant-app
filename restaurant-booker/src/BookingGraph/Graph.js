42b0905bfd64bd645dae111692d2da2e3e5784a2
Serenity911 <silvia.simonassi01@gmail.com>
2020-03-11 15:01:36 +0000

    graph updated

--- restaurant-booker/src/BookingGraph/Graph.js
+++ restaurant-booker/src/BookingGraph/Graph.js
@@ -1,105 +1,128 @@
-import React, { Component, useState } from "react";
-import './Graph.css'
+import React, { Component, useState, useEffect } from "react";
+import "./Graph.css";
+import GraphTableRow from "./GraphTableRow.js";
 
-function Graph ({bookings, bookingSlots, tables}) {
-    const [displayDate, setdisplayDate] = useState("")
-    // const [tables, setTables] = useState([
-    //     {id: 1, name: "table1", pax: 2, priority: 1, duration: 1, accessibility: true, bookings: 
-    //         [{id:1, time: "19:00", date: "01/01/2020", numberOfPeople: 2}]
-    //     },
-    //     {id: 2, name: "table2", pax: 4, priority: 1, duration: 1, accessibility: false, bookings: 
-    //     [{id:3, time: "19:00", date: "06/01/2020", numberOfPeople: 3}, 
-    //     {id:15, time: "21:00", date: "06/01/2020", numberOfPeople: 3}
-    //     ]
-    //     },
-    //     {id: 3, name: "table3", pax: 6, priority: 1, duration: 1, accessibility: false, bookings: 
-    //     []
-    //     },
-    //     {id: 4, name: "table4", pax: 6, priority: 1, duration: 1, accessibility: false, bookings: 
-    //     [{id:16, time: "20:00", date: "06/01/2020", numberOfPeople: 3}]
-    //     }
-    // ]);
+function Graph({ bookings, bookingSlots, tables }) {
+  const [displayDate, setdisplayDate] = useState("2020-03-06");
+  //   const [filteredTablesByDate, setFilteredTablesByDate] = useState([]);
+  // const [tables, setTables] = useState([
+  //     {id: 1, name: "table1", pax: 2, priority: 1, duration: 1, accessibility: true, bookings:
+  //         [{id:1, time: "19:00", date: "01/01/2020", numberOfPeople: 2}]
+  //     },
+  //     {id: 2, name: "table2", pax: 4, priority: 1, duration: 1, accessibility: false, bookings:
+  //     [{id:3, time: "19:00", date: "06/01/2020", numberOfPeople: 3},
+  //     {id:15, time: "21:00", date: "06/01/2020", numberOfPeople: 3}
+  //     ]
+  //     },
+  //     {id: 3, name: "table3", pax: 6, priority: 1, duration: 1, accessibility: false, bookings:
+  //     []
+  //     },
+  //     {id: 4, name: "table4", pax: 6, priority: 1, duration: 1, accessibility: false, bookings:
+  //     [{id:16, time: "20:00", date: "06/01/2020", numberOfPeople: 3}]
+  //     }
+  // ]);
 
-    function getToday(){
-        const date = new Date();
-        
-    }
+  // function getToday(){
+  //     const date = new Date();
 
-    function handleChangeDate(event){
-        return setdisplayDate(event.target.value)
-    }
-    
-    function filterTablesByBookingDate(dateString){
-        return tables.map(table => table.bookings.find(({date}) => date === dateString ))
-    }
+  // }
 
-    const createTD = tables.map(table => 
-        {
-            // console.log(displayDate);
-            
-        // console.log(filterTablesByBookingDate(displayDate));
-        
-        return <tr>
-        <td>Table number: {table.id}</td>
-        {bookingSlots.map(bookingSlot => {
-            if(table.bookings.length === 0) {
-                return <td>No Bookings for this time</td>
-            }
-            else {
-                let findTable = table.bookings.find(({time}) => {
-                    return  bookingSlot >= time && bookingSlot <= getEndTime(time) 
-                }) 
-                if(findTable) {
-                let findCustomer = findTable.customer ? findTable.customer.name : ""                  
-                return <td className="entry"><a href=""> Customer: {findCustomer} <br/> People: {findTable.numberOfPeople}</a></td>
-                }
-                else {
-                return <td>No Bookings for this time</td>
-                }
-            }
-         } 
-         )}
+  function handleChangeDate(event) {
+    return setdisplayDate(event.target.value);
+  }
 
-        </tr>
-        }
-    )
-        
-    function getEndTime(timeString, duration = 1){
-        
-        let timeIncreasedInt = parseInt(timeString.split(":")[0])+ duration 
-        let timeIncreasedInString = `${timeIncreasedInt}:00`
-        return timeIncreasedInString
-    }
+  //   function filterTablesByBookingDate(tables) {
+  //     if (!tables) {
+  //       tables.bookings.filter(({ date }) => {
+  //         return date === displayDate;
+  //       });
+  //     }
+  //   }
 
-        // const mapBooking = tables.map(item => {
-        //     return <p>{item.name} || first booking: {item.bookings.time}</p>
-        // })
+  //   useEffect(() => {
+  //     let tablesFiltered = tables.bookings.filter(({ date }) => {
+  //       return date === displayDate;
+  //     });
+  //     setFilteredTablesByDate(tablesFiltered);
+  //   }, [displayDate, tables]);
 
-    const createHeader = bookingSlots.map(item => {
-    return <th>{item}</th>
-    })
+  //   const createTD = filteredTablesByDate.map(table => {
+  //     // const tablesFilteredByBookingDate = table.bookings.filter(({ date }) => {
+  //     //   return date === displayDate;
+  //     // });
+  //     // console.table(tablesFilteredByBookingDate);
+  //     // console.log("log of tables filtered " + tablesFilteredByBookingDate[0]);
+  //     // console.log(tables);
 
+  //     return (
+  //       <tr>
+  //         <td>Table number: {table.id}</td>
 
-        return(
-            <>
-            <h3>This is the Graph section</h3>
-            <input 
-                type="date" 
-                value={displayDate} 
-                onChange={handleChangeDate}/>
-            <table> 
-                <tbody>
-                    <tr>
-                        <th></th>
-                        {createHeader}
-                    </tr>
-                    {createTD}
-                </tbody>
-            </table>
-            <h4> below map Booking</h4> 
-            {/* {mapBooking} */}
-            </>
-        )
+  //         {bookingSlots.map(bookingSlot => {
+  //           if (table.bookings.length === 0) {
+  //             return <td>No Bookings for this time</td>;
+  //           } else {
+  //             let findTable = table.bookings.find(({ time }) => {
+  //               return bookingSlot >= time && bookingSlot <= getEndTime(time);
+  //             });
+  //             if (findTable) {
+  //               let findCustomer = findTable.customer
+  //                 ? findTable.customer.name
+  //                 : "";
+  //               return (
+  //                 <td className="entry">
+  //                   <a href="">
+  //                     {" "}
+  //                     Customer: {findCustomer} <br /> People:{" "}
+  //                     {findTable.numberOfPeople}
+  //                   </a>
+  //                 </td>
+  //               );
+  //             } else {
+  //               return <td>No Bookings for this time</td>;
+  //             }
+  //           }
+  //         })}
+  //       </tr>
+  //     );
+  //   });
+
+  //   function getEndTime(timeString, duration = 1) {
+  //     let timeIncreasedInt = parseInt(timeString.split(":")[0]) + duration;
+  //     let timeIncreasedInString = `${timeIncreasedInt}:00`;
+  //     return timeIncreasedInString;
+  //   }
+
+  // const mapBooking = tables.map(item => {
+  //     return <p>{item.name} || first booking: {item.bookings.time}</p>
+  // })
+
+  const createHeader = bookingSlots.map(item => {
+    return <th>{item}</th>;
+  });
+
+  return (
+    <>
+      <h3>This is the Graph section</h3>
+      <input type="date" value={displayDate} onChange={handleChangeDate} />
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            {createHeader}
+          </tr>
+          <GraphTableRow
+            tables={tables}
+            bookingSlots={bookingSlots}
+            displayDate={displayDate}
+          />
+          {/* {createTD} */}
+        </tbody>
+      </table>
+      <h4> below map Booking</h4>
+      {/* {mapBooking} */}
+    </>
+  );
 }
 
 export default Graph;
---
+++ restaurant-booker/src/BookingGraph/GraphTableRow.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+
+function GraphTableRow({ bookingSlots, tables, displayDate }) {
+  const [filteredTablesByDate, setFilteredTablesByDate] = useState([]);
+
+  useEffect(() => {
+    let tablesFiltered = tables.filter(({ bookings }) => {
+      const dates = bookings.map(booking => booking.date);
+      return dates.includes(displayDate);
+    });
+    setFilteredTablesByDate(tablesFiltered);
+  }, [displayDate, tables]);
+
+  const createTD = tables.map(table => {
+    // const tablesFilteredByBookingDate = table.bookings.filter(({ date }) => {
+    //   return date === displayDate;
+    // });
+    // console.table(tablesFilteredByBookingDate);
+    // console.log("log of tables filtered " + tablesFilteredByBookingDate[0]);
+    // console.log(tables);
+
+    return (
+      <tr>
+        <td>Table number: {table.id}</td>
+
+        {bookingSlots.map(bookingSlot => {
+          if (table.bookings.length === 0) {
+            return <td>No Bookings for this time</td>;
+          } else {
+            let findTable = table.bookings.find(({ time, date }) => {
+              return (
+                bookingSlot >= time &&
+                bookingSlot <= getEndTime(time) &&
+                date === displayDate
+              );
+            });
+            if (findTable) {
+              let findCustomer = findTable.customer
+                ? findTable.customer.name
+                : "";
+              return (
+                <td className="entry">
+                  <a href="">
+                    {" "}
+                    Customer: {findCustomer} <br /> People:{" "}
+                    {findTable.numberOfPeople}
+                  </a>
+                </td>
+              );
+            } else {
+              return <td>No Bookings for this time</td>;
+            }
+          }
+        })}
+      </tr>
+    );
+  });
+
+  function getEndTime(timeString, duration = 1) {
+    let timeIncreasedInt = parseInt(timeString.split(":")[0]) + duration;
+    let timeIncreasedInString = `${timeIncreasedInt}:00`;
+    return timeIncreasedInString;
+  }
+
+  if (!tables) {
+    return null;
+  } else {
+    return <>{createTD}</>;
+  }
+}
+
+export default GraphTableRow;
