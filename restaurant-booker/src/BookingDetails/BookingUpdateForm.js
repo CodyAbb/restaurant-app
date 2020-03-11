3ef7531e86e842d81b433ed5469e472349f6a130
Serenity911 <silvia.simonassi01@gmail.com>
2020-03-11 11:24:25 +0000

    put is working, we can update

--- restaurant-booker/src/BookingDetails/BookingUpdateForm.js
+++ restaurant-booker/src/BookingDetails/BookingUpdateForm.js
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
+import Axios from "axios";
 
 function BookingUpdateForm({ selectedBooking, bookingSlots, tablesAvailable }) {
-  const [numberOfPeople, setNumberOfPeople] = useState(
-    selectedBooking.numberOfPeople
-  );
+  const [pax, setPax] = useState(selectedBooking.numberOfPeople);
   const [date, setDate] = useState(selectedBooking.date);
   const [time, setTime] = useState(selectedBooking.time);
   const [table, setTable] = useState(selectedBooking.table);
   // const[bookingSlots, setBookingSlots] = useState(bookingSlots)
 
-  function handleNumberOfPeople(event) {
-    setNumberOfPeople(event.target.value);
+  function handlePax(event) {
+    setPax(event.target.value);
   }
 
   function handleDate(event) {
@@ -35,7 +34,7 @@ function BookingUpdateForm({ selectedBooking, bookingSlots, tablesAvailable }) {
   const populateAvailableTableOptions = tablesAvailable.map(tableAvailable => {
     let isItSelected = tableAvailable === table ? "selected" : "";
 
-    if (tableAvailable.pax >= numberOfPeople) {
+    if (tableAvailable.pax >= pax) {
       return (
         <option
           key={tableAvailable.id}
@@ -48,15 +47,45 @@ function BookingUpdateForm({ selectedBooking, bookingSlots, tablesAvailable }) {
     }
   });
 
+  function putUpdatedBooking() {
+    const customer = `http://localhost:8080/customers/${selectedBooking.customer.id}`;
+    const desk = `http://localhost:8080/desks/${selectedBooking.desk.id}`;
+    const numberOfPeople = pax;
+    JSON.stringify({ time, date, numberOfPeople, customer, desk });
+    Axios.put(`http://localhost:8080/bookings/${selectedBooking.id}`, {
+      time,
+      date,
+      numberOfPeople,
+      customer,
+      desk
+    }).then(res => console.log(`the update res is ${res}`));
+  }
+
+  //   patchCustomerIdInBooking() {
+  //     console.log(`${this.state.customer_id}`);
+  //     const customer = `http://localhost:8080/customers/${this.state.customer_id}`;
+
+  //     JSON.stringify({ customer });
+  //     console.log(JSON.stringify({ customer }));
+
+  //     console.log(`http://localhost:8080/bookings/${this.state.newBookingId}`);
+
+  //     Axios.patch(`http://localhost:8080/bookings/${this.state.newBookingId}`, {
+  //       customer: customer
+  //     }).then(res => {
+  //       console.log(`this is the res from the patch ${res}`);
+  //     });
+  //   }
+
   return (
     <>
-      <form>
+      <form onSubmit={putUpdatedBooking}>
         <label for="number of customer">Number of Customer</label>
         <input
           type="number"
           placeholder="Add number of customers"
-          value={numberOfPeople}
-          onChange={handleNumberOfPeople}
+          value={pax}
+          onChange={handlePax}
         />
 
         <label for="date">Date:</label>
