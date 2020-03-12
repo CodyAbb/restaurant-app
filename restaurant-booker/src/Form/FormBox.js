7ea832efddffea32e389cccd1f5db91da77e5efb
Cody Abbott <cody.abbott16@gmail.com>
2020-03-12 10:25:54 +0000

    can generate bookings based on name search

--- restaurant-booker/src/Form/FormBox.js
+++ restaurant-booker/src/Form/FormBox.js
@@ -4,7 +4,7 @@ import Axios from "axios";
 import BookingList from "./BookingList";
 import "./formbox.css";
 
-const creds = require("../config/IdConfig.js");
+const creds = require("../config/Idconfig.js");
 
 class FormBox extends Component {
   constructor({ bookings, bookingSlots }) {
